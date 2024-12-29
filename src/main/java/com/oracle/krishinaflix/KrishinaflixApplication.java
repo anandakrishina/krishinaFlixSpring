8c63b57b806ef822ede7744c1f9e5c56c317c406
krysnand <anandakrishinar@gmail.com>
2024-12-29 20:40:43 -0300

    create model DadosTemporada

--- src/main/java/com/oracle/krishinaflix/KrishinaflixApplication.java
+++ src/main/java/com/oracle/krishinaflix/KrishinaflixApplication.java
@@ -2,11 +2,15 @@ package com.oracle.krishinaflix;
 
 import com.oracle.krishinaflix.model.DadosEpisodio;
 import com.oracle.krishinaflix.model.DadosSeries;
+import com.oracle.krishinaflix.model.DadosTemporada;
 import com.oracle.krishinaflix.service.ConsumirAPI;
 import com.oracle.krishinaflix.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class KrishinaflixApplication implements CommandLineRunner {
@@ -27,6 +31,18 @@ public class KrishinaflixApplication implements CommandLineRunner {
 		json = consumirAPI.obterDados("https://www.omdbapi.com/?t=friends&season=1&episode=2&apikey=8cdb0b9c");
 		var dadosEpisodio = converterDados.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+
+
+		for (int i = 1; i <= dadosSeries.totalTemporadas(); i++){
+			json = consumirAPI.obterDados("https://www.omdbapi.com/?t=friends&season=" + i + "&apikey=8cdb0b9c");
+			DadosTemporada dadosTemporada = converterDados.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 
 	}
 }
